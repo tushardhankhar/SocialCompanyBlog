e72b9535fb5c69ecab388f6413b6f9c6be75b380
tushardhankhar <tushardhankhar98@gmail.com>
2021-08-07 20:20:50 +0530

    User Forms - 1

---
+++ socialcomapnyblog/User/form.py
@@ -0,0 +1,5 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField , SubmitField , PasswordField
+from wtforms.validators import DataRequired , EqualTo , Email
+from flask_wtf.file import FileAllowed , FileField
+
